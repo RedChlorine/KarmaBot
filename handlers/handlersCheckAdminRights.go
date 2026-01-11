5d14ca7fcc717a57d88b31463dd24a1f0829ef84
RedChlorine <vinchelof@yahoo.com>
2026-01-11 18:02:37 +0200

    amend prev

--- handlers/handlersCheckAdminRights.go
+++ handlers/handlersCheckAdminRights.go
@@ -1,12 +1,39 @@
 package handlers
 
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
 // Checks if a user is in the authorised list of admins for super-commands - checks DB for ID
 func CheckAdminRightsSuper(userID int64) bool {
+	// --- PRIORITY CHECK: DB --- //
 	var role string
 	err := DB.QueryRow("SELECT role FROM bot_admins WHERE user_id = $1", userID).Scan(&role)
+
 	if err != nil {
-		LogError("❌ DB ERROR: Could not parse superadmin rights for %d from the DB: %v", userID, err)
-		return false
+		log.Printf("❌ DB ERROR: Could not parse superadmin rights for %d from the DB - FALLBACK to Config.env: %v", userID, err)
+
+		// --- FALLBACK CHECK: Config File (prevent lockout) --- //
+		headAdminIDStr := os.Getenv("HEAD_ADMIN_ID")
+		if headAdminIDStr != "" {
+
+			headAdminID, err := strconv.ParseInt(headAdminIDStr, 10, 64)
+			if err != nil {
+				log.Panicf("[!!PANIC!!]\nHEAD_ADMIN_ID could not be determined! - check config.env for empty strings! - LOCKOUT RISK!")
+				return false
+			}
+			if userID == headAdminID {
+				// Optional: Auto-fix the DB if the config admin is missing
+				go func() {
+					username := os.Getenv("HEAD_ADMIN_USERNAME")
+					DBAddAdmin(headAdminID, username, "superadmin")
+				}()
+				return role == "superadmin" // Authorized via Config File
+			}
+
+		}
 	}
 	return role == "superadmin"
 }
